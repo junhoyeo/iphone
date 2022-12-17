1e93451a5276ac8f3b0fa2801ab26c5b79fb3652
Junho Yeo <i@junho.io>
2022-12-18 01:20:18 +0900

    Make build work

---
+++ src/types/styled-jsx.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="styled-jsx" />
+
+import 'react';
+
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean;
+    global?: boolean;
+  }
+}
